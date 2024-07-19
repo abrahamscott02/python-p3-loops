a533a3b1407ecf901ad3ee2f880d8006aa21469a
Abraham Scott <abrahamscott02@gmail.com>
2024-07-19 16:08:15 -0600

    Donen

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
+import math
 
 def happy_new_year():
-    # code goes here!
-    pass
+    Counter = 10
+    while Counter > 0:
+        print(Counter)
+        Counter = Counter - 1
+    
+    print('Happy New Year!')
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+    for integer in int_list:
+        squared_list.append(integer ** 2)
+    return squared_list
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    count = 1
+    while count < 101:
+        if count % 3 == 0 and count % 5 == 0:
+            print('FizzBuzz')
+        elif count % 3 == 0:
+            print('Fizz')
+        elif count % 5 == 0:
+            print('Buzz')
+        else:
+            print(count)
+        count += 1
